f0a0279d9227658ef6226552741327e036dd4c4c
Riwalenn Bas <riwalenn@gmail.com>
2024-03-11 19:20:04 +0100

    Lessons : unit test

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import main
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        print('about to test a function')
+
     def test_do_stuff(self):
         test_param = 10
         result = main.do_stuff(test_param)
@@ -11,7 +14,20 @@ class TestMain(unittest.TestCase):
     def test_do_stuff2(self):
         test_param = "dsqfdf"
         result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_stuff3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter a number')
+
+    def test_do_stuff4(self):
+        test_param = ""
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter a number')
+
+    def tearDown(self):
+        print('cleaning up')
 
 
 unittest.main()
